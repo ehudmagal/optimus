f02a482be97998df841ec3a4ff22e4f09f59ba53
Ehud <ehudmagal@gmail.com>
2016-04-20 12:20:59 +0300

    angular module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -13,5 +15,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     '/admin_gui'
   end
+
+
 
 end
