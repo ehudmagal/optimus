cc94dc870717f31981bbc1cff0815d20b07718e5
Ehud <ehudmagal@gmail.com>
2016-04-20 11:13:17 +0300

    create order with json

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,4 +22,24 @@ class OrdersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      format.html {super}
+      format.json {
+        order = Order.find params['order']['id']
+        order.update(params['order'])
+        render json: order
+      }
+    end
+  end
+
+  private
+  # Using a private method to encapsulate the permissible parameters
+  # is just a good pattern since you'll be able to reuse the same
+  # permit list between create and update. Also, you can specialize
+  # this method with per-user checking of permissible attributes.
+  def order_params
+    params.require(:order).permit(:source, :destination)
+  end
+
 end
