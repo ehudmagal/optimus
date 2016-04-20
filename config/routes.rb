6930cb6fb077f1d212069a1bd7a1f69fa9ed86b6
Ehud <ehudmagal@gmail.com>
2016-04-20 11:26:36 +0300

    angular module

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'admin_gui/admin_gui'
+
 Rails.application.routes.draw do
+  get '/admin/users/sign_in' => redirect('/users/sign_in')
   root to: 'visitors#index'
   devise_for :users
   resources :users
   resources :orders
+  mount AdminGui => '/admin'
 end
