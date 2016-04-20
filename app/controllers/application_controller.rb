c4aa8afc02ff071c03e8c85e703d23feee0be35f
Ehud <ehudmagal@gmail.com>
2016-04-20 12:17:33 +0300

    angular module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :set_csrf_cookie
+
+  def set_csrf_cookie
+    if protect_against_forgery?
+      cookies.permanent['CSRF-TOKEN'] = form_authenticity_token
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    '/admin_gui'
+  end
+
 end
