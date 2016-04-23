512089c193078bfb9e1c84e959910e177a242b8b
Ehud <ehudmagal@gmail.com>
2016-04-23 14:10:09 +0300

    angular with ruby

--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -2,12 +2,9 @@
 //= require ./vendor/pace
 //= require ./vendor/jquery-1.11.1.min.js
 //= require ./vendor/jquery.cookie
-//= require ./vendor/google-maps-infobubble.js
 //= require ./vendor/angular.min
 //= require ./vendor/angular-cookies.min
 //= require ./vendor/angular-bootstrap.min
-//= require ./vendor/google-maps-graticule
 //= require ./vendor/mobile-angular-ui.min
 //= require ./vendor/mobile-angular-ui.gestures.min
 //= require ./vendor/ng-table.js
-//= require ./vendor/marker_with_label
