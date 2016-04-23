0f4be8dbf03d75b18f19c21defba004f13bc0938
Ehud <ehudmagal@gmail.com>
2016-04-23 20:55:57 +0300

    form for new order

--- db/migrate/20160423175136_add_weightto_orders.rb
+++ db/migrate/20160423175136_add_weightto_orders.rb
@@ -1,4 +1,7 @@
 class AddWeighttoOrders < ActiveRecord::Migration
   def change
+    add_column :orders, :weight, :float
+    add_column :orders, :goods_type, :string
+    add_column :orders, :goods_type, :string
   end
 end
