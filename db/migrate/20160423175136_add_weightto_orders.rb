6d107128c7fe3846bc4a538f433905639f3d4ee4
Ehud <ehudmagal@gmail.com>
2016-04-24 21:00:24 +0300

    add fields to order

--- db/migrate/20160423175136_add_weightto_orders.rb
+++ db/migrate/20160423175136_add_weightto_orders.rb
@@ -3,5 +3,9 @@ class AddWeighttoOrders < ActiveRecord::Migration
     add_column :orders, :weight, :float
     add_column :orders, :goods_type, :string
     add_column :orders, :goods_type, :string
+    add_column :orders, :start_date, :date
+    add_column :orders, :end_date, :date
+    add_column :orders, :tons_per_hour, :float
+    add_column :orders, :method, :string
   end
 end
