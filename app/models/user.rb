4c16bb07ab90546e1801b6eb14aaba2ba8126bef
Ehud <ehudmagal@gmail.com>
2016-06-13 17:36:55 +0300

    role column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+  before_save :default_values
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def default_values
+    if role.nil?
+      role = 'customer'
+    end
+  end
 end
