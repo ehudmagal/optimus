b86d2a226a8b1be757573b82c05df3021867c5c9
Ehud <ehudmagal@gmail.com>
2016-07-26 21:25:30 +0300

    show bids

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,7 +3,6 @@ class Bid < ActiveRecord::Base
   belongs_to :order
   def json_attributes
     json = as_json
-    json['order'] = order.json_attributes
     json
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ActiveRecord::Base
   def json_attributes
     json = as_json
     json['user'] = user
+    json['bids'] = bids.map{|bid| bid.json_attributes}
     json
   end
 end
