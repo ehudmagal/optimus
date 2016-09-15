ede3ea1a492ba16999f5efa4d9b248422eb0ad55
Ehud <ehudmagal@gmail.com>
2016-09-15 22:51:06 +0300

    set bid status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,27 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :bids
+  before_save :set_bids_status
+
   def json_attributes
     json = as_json
     json['user'] = user
-    json['bids'] = bids.map{|bid| bid.json_attributes}
+    json['bids'] = bids.map { |bid| bid.json_attributes }
     json
+  end
+
+  def set_bids_status
+    unless selected_bid_id.nil?
+      over_bids = bids.where.not(id: selected_bid_id)
+      cancelled_bids.each do |bid|
+        bid.update status: 'over'
+      end
+      selected_bid = bids.where(id: selected_bid_id).first
+      selected_bid.update status: 'selected'
+    else
+      bids.each do |bid|
+        bid.update status: 'pending'
+      end
+    end
   end
 end
