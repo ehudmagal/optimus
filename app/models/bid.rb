bbbb86ad2373d8bd8d3574653af900c7f9d2ef25
Ehud <ehudmagal@gmail.com>
2016-09-20 21:11:45 +0300

    default value for bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,9 +1,15 @@
 class Bid < ActiveRecord::Base
   belongs_to :user
   belongs_to :order
+  before_save :default_values
+
   def json_attributes
     json = as_json
     json['user'] = user
     json
+  end
+  private
+  def default_values
+    self.status ||= 'pending'
   end
 end
