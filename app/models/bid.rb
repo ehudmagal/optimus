f12363b6363b76d4bdeb360eaa6c324c18954987
Ehud <ehudmagal@gmail.com>
2016-09-22 09:00:49 +0300

    change order status to approved after bid is selected

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,7 +2,8 @@ class Bid < ActiveRecord::Base
   belongs_to :user
   belongs_to :order
   before_save :default_values
-  after_create :set_order_status
+  before_save :set_order_status
+  STATUSES = {pending: 'pending', over: 'over',selected: 'selected'}
   def json_attributes
     json = as_json
     json['user'] = user
@@ -10,12 +11,15 @@ class Bid < ActiveRecord::Base
   end
   private
   def default_values
-    self.status ||= 'pending'
+    self.status ||= STATUSES[:pending]
   end
 
   def set_order_status
     if order.status == Order::STATUSES[:booked]
       order.update status: Order::STATUSES[:bidded]
+    end
+    if status == STATUSES[:selected]
+      order.update status: Order::STATUSES[:approved]
     end
   end
 end
