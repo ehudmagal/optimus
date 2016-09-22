dbbbeb25c1cdc433f3e8864969d4321f077ba02a
Ehud <ehudmagal@gmail.com>
2016-09-22 09:31:33 +0300

    globals for order and bid status

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,8 +1,7 @@
 class Bid < ActiveRecord::Base
   belongs_to :user
   belongs_to :order
-  before_save :default_values
-  before_save :set_order_status
+  after_create :default_values
   STATUSES = {pending: 'pending', over: 'over',selected: 'selected'}
   def json_attributes
     json = as_json
@@ -11,15 +10,6 @@ class Bid < ActiveRecord::Base
   end
   private
   def default_values
-    self.status ||= STATUSES[:pending]
-  end
-
-  def set_order_status
-    if order.status == Order::STATUSES[:booked]
-      order.update status: Order::STATUSES[:bidded]
-    end
-    if status == STATUSES[:selected]
-      order.update status: Order::STATUSES[:approved]
-    end
+    self.update status: STATUSES[:pending]
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :bids
-  before_save :set_bids_status
+  after_save :set_bids_status
   after_create :set_default_status
-  STATUSES = {booked: 'booked', bidded: 'bidded',approved: 'approved',
+  STATUSES = {booked: 'booked', bidded: 'bidded', approved: 'approved',
               before_pickup: 'before_pickup', picked_up: 'picked_up', delivered: 'delivered'}
+
   def json_attributes
     json = as_json
     json['user'] = user
@@ -16,20 +17,21 @@ class Order < ActiveRecord::Base
     unless selected_bid_id.nil?
       over_bids = bids.where.not(id: selected_bid_id)
       over_bids.each do |bid|
-        bid.update status: 'over'
+        bid.update status:  Bid::STATUSES[:over]
       end
       selected_bid = bids.where(id: selected_bid_id).first
-      selected_bid.update status: 'selected'
+      selected_bid.update status: Bid::STATUSES[:selected]
+      self.update status: STATUSES[:approved]
     else
       bids.each do |bid|
-        bid.update status: 'pending'
+        bid.update status:  Bid::STATUSES[:pending]
       end
     end
   end
 
   private
   def set_default_status
-    self.status = STATUSES[:booked]
+    self.update status: STATUSES[:booked]
   end
 
 end
