91e37cf41f2dcab39b4b145e22513f60ea78bea8
Ehud <ehudmagal@gmail.com>
2016-10-09 09:24:38 +0300

    cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  ROLES = {customer: 'customer',
+           supplier: 'supplier',
+           dispatcher: 'dispatcher',
+           admin: 'admin'}
 
   def default_values
     if role.nil?
