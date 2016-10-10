8f5fb7a97701ae877ba87196ae4da3cdc817434f
ehudmagal <ehudmagal@gmail.com>
2016-10-10 14:15:03 -0400

    activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 require 'admin_gui/admin_gui'
 require 'mobile_navigator_gui/mobile_navigator_gui'
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   get '/admin/users/sign_in' => redirect('/users/sign_in')
   get '/mobile_navigator/users/sign_in' => redirect('/users/sign_in')
   mount AdminGui => '/admin'
   mount MobileNavigatorGui => '/mobile_navigator'
   root 'main#index'
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :users
   ActiveAdmin.routes(self)
   resources :users do
     collection do
