78c1f6ef0d380ee03e29e381f3896bb729d46358
Ehud <ehudmagal@gmail.com>
2016-10-22 18:27:57 +0300

    mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,14 +27,17 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV['gmail_username'],
+      :password             => ENV['gmail_password'],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
