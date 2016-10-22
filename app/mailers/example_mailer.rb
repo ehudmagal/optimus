1a46f54d811f9e9bb8d2c7d9acbb9619163d3ce3
Ehud <ehudmagal@gmail.com>
2016-10-22 18:14:10 +0300

    email

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,2 +1,8 @@
-class ExampleMailer < ApplicationMailer
+class ExampleMailer < ActionMailer::Base
+  default from: "ehudmagal@optimus.ninja"
+
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
 end
