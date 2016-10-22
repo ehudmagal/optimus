7af0ec0ebfe56635edd7c7e360c611401cf34b5d
Ehud <ehudmagal@gmail.com>
2016-10-22 20:07:52 +0300

    send email to driver if order status is changed to accepted

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
-      :port                 => 587,
+      :port                 => 465,
       :domain               => 'gmail.com',
       :user_name            => ENV['gmail_username'],
       :password             => ENV['gmail_password'],
