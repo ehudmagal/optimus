9b29591ddd05eb3abdb4a2c47513ea1b3c856f27
Ehud Magal <ehudmagal@gmail.com>
2016-11-19 23:10:06 +0200

    fix signup bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
-  before_action :authenticate_user!
+  before_action :authenticate_user!  ,only: [:show, :num_of_closed_bids_with_user, :index]
   def index
     @users = User.all
   end
@@ -21,13 +21,20 @@ class UsersController < ApplicationController
     render json: orders.count
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: {success: true}
+    else
+      render_json_errors  ['shalom']
+    end
+  end
+
   protected
 
-  def configure_devise_permitted_parameters
-    registration_params = [:name, :email, :password, :password_confirmation, :role]
-    devise_parameter_sanitizer.for(:sign_up) {
-        |u| u.permit(registration_params)
-    }
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
   end
 
 end
