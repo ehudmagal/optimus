57e3deac5304b20abd2f0751fe1b4ac0c28db941
Ehud Magal <ehudmagal@gmail.com>
2016-11-26 17:46:02 +0200

    shalom

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,16 +71,20 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
+  #SMTP settings for gmail
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => 'ehudmagal@gmail.com',
-      :password             => 'uzliguzli1',
+      :user_name            => ENV['gmail_username'],
+      :password             => ENV['gmail_password'],
       :authentication       => "plain",
+      :domain => ENV['domain'],
       :enable_starttls_auto => true
   }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
