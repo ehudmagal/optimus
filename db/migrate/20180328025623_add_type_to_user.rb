4324d2a1a10f59797fffd766d75ca5889d78db1e
Ehud Magal <ehudmagal@gmail.com>
2018-03-28 06:19:00 +0300

    driver

--- db/migrate/20180328025623_add_type_to_user.rb
+++ db/migrate/20180328025623_add_type_to_user.rb
@@ -1,6 +1,8 @@
 class AddTypeToUser < ActiveRecord::Migration
   def up
     add_column :users, :type, :string
+    users = User.select{|u| u.role == 'supplier'}
+    users.map{|u| u.update type: 'Driver'}
   end
 
   def down
