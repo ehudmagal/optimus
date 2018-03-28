26b09f70c33aab3b0902b765a63d2216c4273831
Ehud Magal <ehudmagal@gmail.com>
2018-03-28 07:01:24 +0300

    new module for users

---
+++ app/models/users/driver.rb
@@ -0,0 +1,5 @@
+module Users
+  class Driver < User
+
+  end
+end
---
+++ app/models/users/user.rb
@@ -0,0 +1,29 @@
+module Users
+
+  class User < ActiveRecord::Base
+    before_save :default_values
+    has_many :orders
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable,
+           :recoverable, :rememberable, :trackable, :validatable
+    ROLES = {customer: 'customer',
+             driver: 'driver',
+             drivers_company: 'drivers_company',
+             dispatcher: 'dispatcher',
+             admin: 'admin'}
+
+    def default_values
+      if role.nil?
+        self.role = 'customer'
+      end
+    end
+
+
+    def active_adminable?
+      role == ROLES[:admin]
+    end
+
+
+  end
+end
