9516c3d49503abd279e77ab76e59e4e063a4a482
Ehud Magal <ehudmagal@gmail.com>
2018-03-28 06:46:34 +0300

    sti

--- db/migrate/20180328025623_add_type_to_user.rb
+++ db/migrate/20180328025623_add_type_to_user.rb
@@ -1,12 +1,18 @@
 class AddTypeToUser < ActiveRecord::Migration
   def up
     add_column :users, :type, :string
-    users = User.select{|u| u.role == 'supplier'}
-    users.map{|u| u.update type: 'Driver'}
+    update_users_type('supplier','Driver')
+    update_users_type('customer','Customer')
   end
 
   def down
     remove_column :users, :type
+  end
+
+
+  def update_users_type(role, type)
+    users = User.select{|u| u.role == role}
+    users.map{|u| u.update type: type}
   end
 
 end
