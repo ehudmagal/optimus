207a4732098a6a55b3fd2d85f3ab2b2c9280bd82
Ehud Magal <ehudmagal@gmail.com>
2018-03-30 15:28:59 +0300

    shalom

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.active_adminable?
+    if user.is_a? Admin
       can :manage, :all
     else
       #orders
