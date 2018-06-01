d5b9a87f9e006c0c851e5ae2d2957bcfa9c6c28e
Ehud Magal <ehudmagal@gmail.com>
2018-06-01 10:25:23 +0300

    remove breakpoint

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,7 +27,6 @@ class OrdersController < ApplicationController
     respond_to do |format|
       format.html { super }
       format.json {
-        binding.pry
         order = Order.create(order_params)
         order.update(user_id: current_user.id,
                      source: params['order']['source'],
