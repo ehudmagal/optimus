62fd555a214090a685447286580e1b4f49a0917b
Ehud Magal <ehudmagal@gmail.com>
2018-08-06 06:48:09 +0300

    create payments table

--- db/migrate/20180806034340_create_payments.rb
+++ db/migrate/20180806034340_create_payments.rb
@@ -1,8 +1,8 @@
 class CreatePayments < ActiveRecord::Migration
   def change
     create_table :payments do |t|
-      t.references :customer, index: true, foreign_key: true
-      t.references :driver, index: true, foreign_key: true
+      t.integer :customer_id, index: true
+      t.integer :driver_id, index: true
       t.float :sum
       t.jsonb :option
     end
