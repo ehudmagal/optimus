9829edf096e36826fcbbb458f87dd11ac6b809e2
Ehud Magal <ehudmagal@gmail.com>
2018-08-08 23:35:43 +0300

    order dialog

--- db/migrate/20180806034340_create_payments.rb
+++ db/migrate/20180806034340_create_payments.rb
@@ -2,7 +2,6 @@ class CreatePayments < ActiveRecord::Migration
   def change
     create_table :payments do |t|
       t.integer :order_id, index: true
-      t.float :sum
       t.jsonb :option
     end
   end
