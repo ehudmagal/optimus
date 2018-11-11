135ca8d5093cdbaad41ab9671e342d7a34ee56d8
Ehud <ehudmagal@gmail.com>
2018-11-11 15:32:52 +0200

    wrap in module

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,4 +1,6 @@
-def convert_real_utc_to_local_utc(real_utc, timezone)
-  offset = real_utc.in_time_zone(timezone).utc_offset
-  real_utc + offset
+module Utils
+  def convert_real_utc_to_local_utc(real_utc, timezone)
+    offset = real_utc.in_time_zone(timezone).utc_offset
+    real_utc + offset
+  end
 end
