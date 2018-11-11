7fa6afee95552378d570b299477795aca06471ab
Ehud <ehudmagal@gmail.com>
2018-11-11 15:16:57 +0200

    autoload

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,6 @@ module RailsDevise
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += %W(#{Rails.root}/lib)
   end
 end
