1e98410630abbb1c83b143869ea46017b5b7aaec
Ehud <ehudmagal@gmail.com>
2018-11-11 15:05:47 +0200

    add timezone to users

--- db/migrate/20181111125600_add_timezone_to_users.rb
+++ db/migrate/20181111125600_add_timezone_to_users.rb
@@ -1,5 +1,5 @@
 class AddTimezoneToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :timezone, :string
+    add_column :users, :timezone, :string, default: 'Asia/Jerusalem'
   end
 end
---
+++ lib/utils.rb
@@ -0,0 +1,4 @@
+def convert_real_utc_to_local_utc(real_utc, timezone)
+  offset = real_utc.in_time_zone(timezone).utc_offset
+  real_utc + offset
+end
