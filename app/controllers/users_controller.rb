f44ff8b670e007fa0661a0d0fa68b6dea945e3db
Ehud Magal <ehudmagal@gmail.com>
2019-01-10 09:14:28 +0200

    login user after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in(@user)
       render json: {success: true}
     else
       render_json_errors  ['shalom']
